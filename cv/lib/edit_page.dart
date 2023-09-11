9c9b928c090a6c40638fb40e471f074f2b111fc5
demigod <funmad247@gmail.com>
2023-09-11 18:00:35 +0100

    changed the edit screen

--- cv/lib/edit_page.dart
+++ cv/lib/edit_page.dart
@@ -20,60 +20,65 @@ class _EditPageState extends State<EditPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Edit Resume Details'),
-        backgroundColor: const Color.fromARGB(255, 33, 117, 243),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              SizedBox(
-                height: 200,
-                child: Form(
-                  key: _formKey,
-                  child: TextFormField(
-                    maxLines: 50,
-                    // controller: _controller,
-                    initialValue: widget.value,
-                    onSaved: (newValue) {
-                      setState(() {
-                        widget.value = newValue!;
-                      });
-                    },
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Edit Resume Details'),
+          backgroundColor: const Color.fromARGB(255, 33, 117, 243),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      maxLines: 50,
+                      // controller: _controller,
+                      initialValue: widget.value,
+                      onSaved: (newValue) {
+                        setState(() {
+                          widget.value = newValue!;
+                        });
+                      },
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              GestureDetector(
-                onTap: _save,
-                child: Container(
-                  width: 100,
-                  height: 50,
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      color: Colors.blue[100],
-                      borderRadius: BorderRadius.circular(30)),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Save',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 33, 117, 243),
-                            fontWeight: FontWeight.bold,
-                          )),
-                      Icon(
-                        Icons.add,
-                        color: Color.fromARGB(255, 33, 117, 243),
-                      ),
-                    ],
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: _save,
+                  child: Container(
+                    width: 100,
+                    height: 50,
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Colors.blue[100],
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Save',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 33, 117, 243),
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Icon(
+                          Icons.add,
+                          color: Color.fromARGB(255, 33, 117, 243),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
